3f9b0b00bbaed0d7093410b53ead9177fe08a4ec
Ace <ace.github@gmail.com>
2014-11-01 19:39:07 -0700

    Added uploading function

--- s3.py
+++ s3.py
@@ -1,4 +1,17 @@
+"""
+This script requires that you have the proper key and ids in place,
+Which are obviously not going to be included in a public GitHub repo.
+"""
+
+
 import boto3
 s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-	print(bucket.name)
+
+def view_buckets():
+	for bucket in s3.buckets.all():
+		print(bucket.name)
+
+def upload(bucket, *files):
+	for items in files:
+		data = open(items, 'rb')
+		s3.Bucket(bucket).put_object(Key=items, Body=data)
