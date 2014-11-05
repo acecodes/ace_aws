2d4c3832d8edd6f3c9623f35b0a505c166c74aa8
Ace <ace.github@gmail.com>
2014-11-04 19:33:33 -0800

    Added bucket delete function to S3 module

--- ace_s3.py
+++ ace_s3.py
@@ -1,10 +1,15 @@
 """
 This script requires that you have the proper key and ids in place,
-Which are obviously not going to be included in a public GitHub repo.
+which are obviously not going to be included in a public GitHub repo.
 """
-from itertools import chain
+
 from os import system
-import boto3
+
+try:
+	import boto3
+except ImportError:
+	print("You're missing the boto3 library. Please install and try again.")
+
 s3 = boto3.resource('s3')
 
 def view_buckets():
@@ -35,3 +40,9 @@ def sync(src, dst, src_bucket=False):
 	else:
 		system("aws s3 sync s3://{src} s3://{dst}".format(src=src, dst=dst))
 		print("S3 buckets '{src}' and '{dst}' synced successfully".format(src=src, dst=dst))
+
+def delete_bucket(bucket):
+	"""
+	Delete an empty S3 bucket. Will not work if bucket has objects in it.
+	"""
+	system("aws s3 delete-bucket --bucket {bucket}".format(bucket=bucket))
